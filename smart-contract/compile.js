6748a21c0f230761ac270c4ea1c8fb4571b037ba
Fabian Stiehle <f.stiehle@gmail.com>
2020-01-30 21:45:56 +0100

    improve compiler

--- smart-contract/compile.js
+++ smart-contract/compile.js
@@ -4,11 +4,13 @@
 const solc = require('solc');
 const fs = require('fs');
 
+const CONTRACT = 'RentFactory.sol';
+
 const inputs = {
   language: 'Solidity',
   sources: {
-    'RentFactory.sol': {
-      content: fs.readFileSync('RentFactory.sol').toString()
+    CONTRACT: {
+      content: fs.readFileSync(CONTRACT).toString()
     }
   },
   settings: {
@@ -27,7 +29,14 @@ function findImports(path) {
   }
 }
 
-const compiledCode = solc.compile(JSON.stringify(inputs), { import: findImports })
+const compiledCode = JSON.parse(solc.compile(JSON.stringify(inputs), { import: findImports }))
+
+if (compiledCode.hasOwnProperty('errors')) {
+  for (var error of compiledCode.errors) {
+    console.log(error.formattedMessage);
+  }
+  return;
+}
 
 fs.writeFile('compiled.json', compiledCode, function(err) {
   if (err) throw err;
