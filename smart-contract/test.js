1ce0508389c2cd187d4be215d9fbc0c650f2d46a
Fabian Stiehle <f.stiehle@gmail.com>
2020-02-01 18:58:25 +0100

    impl contract active test

--- smart-contract/test.js
+++ smart-contract/test.js
@@ -7,8 +7,8 @@ const runTest = async (contract, provider, web3, rentContractAbi) => {
   
   console.log("Create new rent contract")
   await contract.methods.createInstance(
-    provider.addresses[2],
     provider.addresses[1],
+    provider.addresses[2],
     500000,
     Date.now(),
     new Date(Date.now()).setFullYear(new Date(Date.now()).getFullYear() + 1),
@@ -27,15 +27,48 @@ const runTest = async (contract, provider, web3, rentContractAbi) => {
   console.log("Test Rent contract")
   const rentContract = new web3.eth.Contract(rentContractAbi, address)
 
-  rentContract.methods.currentSettlementStatus().call({
-    from: provider.addresses[0] 
+  await rentContract.methods.currentSettlementStatus().call({
+    from: provider.addresses[0],
   })
   .then(
     (e) => {
       console.log(e)
     },
-    (error) => console.log(error))
+    (error) => console.log(error));
 
+  console.log("Accept contract...")
+  await rentContract.methods.acceptContract().send({
+    from: provider.addresses[2],
+    value: 500000
+  });
+
+  console.log("Check new settlement status...")
+  await rentContract.methods.currentSettlementStatus().call({
+    from: provider.addresses[0],
+  })
+  .then(
+    (e) => {      
+      assert(e[1][0] == 500000)
+      console.log('OK!');
+    },
+    (error) => {
+      console.log(error);
+      assert.ok(false);
+    });
+
+    console.log("Check contract now active...")
+    await rentContract.methods.active().call({
+      from: provider.addresses[0],
+    })
+    .then(
+      (e) => {
+        console.log("Contract now active? " + e);
+        assert(true == e);
+      },
+      (error) => {
+        console.log(error);
+        assert.ok(false);
+      });
 }
 
 module.exports = runTest;
